3c8a1c3d01cf8178db6c8a72abdf1bc3a5152b07
Om Singh Mehta <140808574+Omsinghmehta@users.noreply.github.com>
2025-08-24 17:38:03 +0530

    Update main.jsx

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./index.css";
 import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
       <App />
+      <Toaster position="top-centre" reverseOrder={false} />
     </StrictMode>
   </Provider>
 );
